030d76d91ba403bda2503ee143f6ab38ac768cb6
carloseduardo2022 <107262978+carloseduardo2022@users.noreply.github.com>
2024-03-20 11:18:22 -0300

    Add files via upload
    20/03

--- main.js
+++ main.js
@@ -1,12 +1,13 @@
 const botoes = document.querySelectorAll(".botao");
+
 for(let i=0;i <botoes.length;i++){
-botoes[i].onclick = function()
-{
-for(let j=0;j <botoeslegth;j++){
-   botoes[j].
-  }
+botoes[i].onclick = function(){
+   
+  for(let j=0;j <botoeslegth;j++){
+   botoes[j].classList.remove("ativo");
+    }
 
-
+   botoes[i].classList.add("ativo");
  }
 
 }
